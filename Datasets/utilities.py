b0203ac18733a7d7259c6f6e7560b140052fe3aa
wtheisen <wtheisen@nd.edu>
2026-01-16 11:27:10 -0500

    Initial shiz for L03

---
+++ Datasets/utilities.py
@@ -0,0 +1,15 @@
+def get_file_str(source_file): # Define a function named 'get_lines_str' that takes an argument called 'source_file'
+    zoomer_text = '' # Create a variable named 'lines' points to a list data structure
+    with open(source_file) as f:
+        for line in f: # For each item in the variable 'f', set the variable named 'line' equal to it, one by one
+            zoomer_text += line.strip() # Add the content of the variable 'line' to the end of the list named 'lines' (after removing whitespace and newlines on either end)
+
+    return zoomer_text # This function returns the variable named 'lines'
+
+def get_lines_str(source_file): # Define a function named 'get_lines_str' that takes an argument called 'source_file'
+    lines = [] # Create a variable named 'lines' points to a list data structure
+    with open(source_file) as f:
+        for line in f: # For each item in the variable 'f', set the variable named 'line' equal to it, one by one
+            lines.append(line.strip()) # Add the content of the variable 'line' to the end of the list named 'lines' (after removing whitespace and newlines on either end)
+
+    return lines # This function returns the variable named 'lines'
