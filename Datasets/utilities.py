e740693dd91fde86753d6a1c007f7ec013780c35
wtheisen <wtheisen@nd.edu>
2026-01-20 14:40:04 -0500

    uts werk

--- Datasets/utilities.py
+++ Datasets/utilities.py
@@ -18,7 +18,7 @@ def get_file_as_list_strs(source_file): # Define a function named 'get_lines_str
     lines = [] # Create a variable named 'lines' points to a list data structure
     with open(source_file) as f:
         for line in f: # For each item in the variable 'f', set the variable named 'line' equal to it, one by one
-            lines.append(line.strip()) # Add the content of the variable 'line' to the end of the list named 'lines' (after removing whitespace and newlines on either end)
+            lines.append('<|sos|>' + line.strip() + '<|eos|>') # Add the content of the variable 'line' to the end of the list named 'lines' (after removing whitespace and newlines on either end)
 
     return lines # This function returns the variable named 'lines'
 
