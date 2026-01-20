51846aaf4d3b9ba497b75c5e10ac8cd6e755696a
wtheisen <wtheisen@nd.edu>
2026-01-20 10:27:24 -0500

    more uts

--- Datasets/utilities.py
+++ Datasets/utilities.py
@@ -14,6 +14,14 @@ def get_file_as_list(source_file): # Define a function named 'get_lines_str' tha
 
     return lines # This function returns the variable named 'lines'
 
+def get_file_as_list_strs(source_file): # Define a function named 'get_lines_str' that takes an argument called 'source_file'
+    lines = [] # Create a variable named 'lines' points to a list data structure
+    with open(source_file) as f:
+        for line in f: # For each item in the variable 'f', set the variable named 'line' equal to it, one by one
+            lines.append(line.strip()) # Add the content of the variable 'line' to the end of the list named 'lines' (after removing whitespace and newlines on either end)
+
+    return lines # This function returns the variable named 'lines'
+
 from collections import defaultdict, Counter
 
 def build_graph_word(source_file, graph=None):
@@ -33,7 +41,7 @@ def build_graph_word(source_file, graph=None):
     return graph
 
 def build_graph_char(source_file, graph=None):
-    lines = list(get_file_as_list(source_file))
+    lines = get_file_as_list_strs(source_file)
 
     if not graph:
         graph = defaultdict(Counter)
